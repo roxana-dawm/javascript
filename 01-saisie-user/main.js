be65a0827bc5dbbbad873b9d92dbaa09a06bdc79
roxana-dawm <roxanamir75@gmail.com>
2020-06-16 10:18:36 +0200

    commit 01-saisie-user

---
+++ 01-saisie-user/main.js
@@ -0,0 +1,41 @@
+
+
+//affiche dans la console
+//console.log("test de connexion!");
+
+//Pour afficher des informations--> alerte
+//alert("Bonjour JavaScript depuis la boite alerte")
+
+// Boîte de dialogue pour confirmation
+//let reponse= confirm("voulez vous confirmez votre prenom")
+//console.log(reponse;)
+
+//Saisie utilisateur -->prompt
+//let.prenom =prompt("entrer votre prenom svp");
+//console.log(prenom);
+//console.log(typeof prenom);
+
+
+//Tout ce que vous saissisez dans le prompt est considéré comme"string"
+//let age=prompt("Entrez votre âge");
+//console.log(age);
+//console.log(typeof age);
+
+//let.prenom =prompt("entrer votre prenom svp");
+//let age=prompt("Entrez votre âge");
+//alert("bonjour"+prenom+",vous avez"+age+"ans");
+
+
+//Anti slash pour permettre a l'apostrophe de ne pas être confondu
+//console.log('Dimanche j\'irais aux "USA"');
+//Et si on veut utiliser des doubles guillemets on doit mettre un anti slash(protection/échappement)pour garder le caractere special ou bien on utilise des templates de string (altgr+7)
+//console.log(`Dimanche j'irais au "USA"`)
+
+//Autres syntaxe (template string) pour faire passer un message "plus facile" et l'anti slash+n=new line pour sauter une ligne
+console.log(`Bonjour ${prenom}\nvous avez ${age}âge`);
+
+
+
+
+
+
