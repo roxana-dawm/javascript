3990a9f395f040155765ef451e91300989cad26d
roxana-dawm <roxanamir75@gmail.com>
2020-06-25 16:52:19 +0200

    commit exercices

---
+++ exercices/partie3.js
@@ -0,0 +1,51 @@
+//partie 4
+//1
+let (maChaine1)='-';
+for( i=0; i<50; i++){
+if ()
+}
+
+console.log("maChaine1");
+
+maChaine1=maChaine1.replace("-","*");
+
+
+//2
+
+let maChaine1 = "*";
+let rev = maChaine1.split('').reverse().join('');
+console.log(rev);
+
+//3
+
+
+
+
+
+
+
+
+//partie 4
+/*
+compteur = 1;
+affichage = "";
+
+do {
+    affichage += `${compteur} `;
+    compteur++;
+} while (compteur <= 1000);
+console.log(affichage);
+
+
+let result = '';
+let i = 0;
+
+do {
+    i = i + 1;
+    result = result + i;
+} while (i < 1000);
+
+console.log(result);
+
+
+
